f0aed6bfc73962d3fc9dde799e9e16181d1a588e
Gary-Lu-IT <reimukaitoku@gmail.com>
2025-05-06 10:56:12 +0800

    變更方案檔檔名，加入AI自動產生套用Dapper的對資料庫CRUD程式碼

---
+++ 基督教會會員管理DAL類別庫/SQLiteDapper/SQLiteDapper.cs
@@ -0,0 +1,171 @@
+﻿namespace ChristianChurchMemberDAL.SQLiteDapper
+{
+    /// <summary>
+    /// 活動資料
+    /// </summary>
+    public class ActivityBasis
+    {
+        /// <summary>
+        /// 活動ID
+        /// </summary>
+        public int ACTIVITYID { get; set; }
+        /// <summary>
+        /// 活動資料設定ID
+        /// </summary>
+        public int ACTIVITYSETTINGID { get; set; }
+        /// <summary>
+        /// 開始日期時間
+        /// </summary>
+        public DateTime STARTDATETIME { get; set; }
+        /// <summary>
+        /// 結束日期時間
+        /// </summary>
+        public DateTime ENDDATETIME { get; set; }
+        /// <summary>
+        /// 主持人或講員
+        /// </summary>
+        public string TEACHER { get; set; }
+    }
+    /// <summary>
+    /// 活動資料設定
+    /// </summary>
+    public class ActivitySetting
+    {
+        /// <summary>
+        /// 活動設定ID
+        /// </summary>
+        public int ACTIVITYSETTINGID { get; set; }
+        /// <summary>
+        /// 活動名稱
+        /// </summary>
+        public string NAME { get; set; }
+        /// <summary>
+        /// 是否定期舉行 (ONCE:僅一次,DAILY:每日,WEEK:每周,MONTH:每月,YEAR:每年)
+        /// </summary>
+        public string IS_RECURRING { get; set; }
+        /// <summary>
+        /// 舉辦日(ONCE填完整日期,WEEK填入星期幾,MONTH填入日期,YEAR填入幾月幾日,DAILY不適用)
+        /// </summary>
+        public string HOLDDAY { get; set; }
+        /// <summary>
+        /// 開始時間
+        /// </summary>
+        public string START_TIME { get; set; }
+        /// <summary>
+        /// 結束時間
+        /// </summary>
+        public string END_TIME { get; set; }
+    }
+    /// <summary>
+    /// 小組成員資料
+    /// </summary>
+    public class GroupMembers
+    {
+        /// <summary>
+        /// 小組ID
+        /// </summary>
+        public string GROUPID { get; set; }
+        /// <summary>
+        /// 小組成員ID
+        /// </summary>
+        public string MEMBERID { get; set; }
+        /// <summary>
+        /// 加入時間
+        /// </summary>
+        public DateTime JOINDATE { get; set; }
+        /// <summary>
+        /// 小組成員層級
+        /// </summary>
+        public int GROUPMEMBERLEVEL { get; set; }
+    }
+    /// <summary>
+    /// 小組資料
+    /// </summary>
+    public class Groups
+    {
+        /// <summary>
+        /// 小組ID
+        /// </summary>
+        public string GROUPID { get; set; }
+        /// <summary>
+        /// 小組名稱
+        /// </summary>
+        public string GROUPNAME { get; set; }
+        /// <summary>
+        /// 組長會員ID
+        /// </summary>
+        public string GROUPMANAGER { get; set; }
+    }
+    /// <summary>
+    /// 會員資料
+    /// </summary>
+    public class Members
+    {
+        /// <summary>
+        /// 會員ID
+        /// </summary>
+        public string MEMBERID { get; set; }
+        /// <summary>
+        /// 會員姓名
+        /// </summary>
+        public string MEMBERNAME { get; set; }
+        /// <summary>
+        /// 生日
+        /// </summary>
+        public DateTime? BIRTHDAY { get; set; }
+        /// <summary>
+        /// 住家郵遞區號
+        /// </summary>
+        public int? ZIPCODE { get; set; }
+        /// <summary>
+        /// 住家地址
+        /// </summary>
+        public string HOMEADDRESS { get; set; }
+        /// <summary>
+        /// 住家電話
+        /// </summary>
+        public string HOMETEL { get; set; }
+        /// <summary>
+        /// 手機號碼
+        /// </summary>
+        public string MOBILETEL { get; set; }
+        /// <summary>
+        /// 電子郵件信箱
+        /// </summary>
+        public string EMAIL { get; set; }
+        /// <summary>
+        /// 信仰層級
+        /// </summary>
+        public int BELIEFSTATUS { get; set; }
+        /// <summary>
+        /// 登入密碼
+        /// </summary>
+        public string LOGINPASSWORD { get; set; }
+        /// <summary>
+        /// 性別 1男0女
+        /// </summary>
+        public int SEX { get; set; }
+    }
+    /// <summary>
+    /// 狀態表
+    /// </summary>
+    public class Statuses
+    {
+        /// <summary>
+        /// 狀態ID
+        /// </summary>
+        public string ITEMNAME { get; set; }
+        /// <summary>
+        /// 狀態值
+        /// </summary>
+        public string ITEMVALUE { get; set; }
+        /// <summary>
+        /// 狀態描述
+        /// </summary>
+        public string ITEMDESCRIPTION { get; set; }
+        /// <summary>
+        /// 套用此設定值的會員是否有系統管理員權限(部分類別不適用)
+        /// </summary>
+        public int ADMINPOWER { get; set; }
+    }
+}
---
+++ 基督教會會員管理Logic端/SQLiteLayer/SQLiteLayer.cs
@@ -0,0 +1,267 @@
+﻿using System.Data.SQLite;
+using ChristianChurchMemberDAL.SQLiteDapper;
+using Dapper;
+
+namespace ChristianChurchMemberLogic.SQLiteLayer
+{
+    internal class SQLiteLayer
+    {
+        private readonly string _connectionString;
+
+        public SQLiteLayer(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public SQLiteConnection GetConnection()
+        {
+            return new SQLiteConnection(_connectionString);
+        }
+
+        // ACTIVITYBASIS CRUD Operations
+        public IEnumerable<ActivityBasis> GetAllActivityBasis()
+        {
+            using (var connection = GetConnection())
+            {
+                return connection.Query<ActivityBasis>("SELECT * FROM ACTIVITYBASIS");
+            }
+        }
+
+        public ActivityBasis GetActivityBasisById(int id)
+        {
+            using (var connection = GetConnection())
+            {
+                return connection.QueryFirstOrDefault<ActivityBasis>("SELECT * FROM ACTIVITYBASIS WHERE ACTIVITYID = @Id", new { Id = id });
+            }
+        }
+
+        public void InsertActivityBasis(ActivityBasis activity)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Execute("INSERT INTO ACTIVITYBASIS (ACTIVITYSETTINGID, STARTDATETIME, ENDDATETIME, TEACHER) VALUES (@ACTIVITYSETTINGID, @STARTDATETIME, @ENDDATETIME, @TEACHER)", activity);
+            }
+        }
+
+        public void UpdateActivityBasis(ActivityBasis activity)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Execute("UPDATE ACTIVITYBASIS SET ACTIVITYSETTINGID = @ACTIVITYSETTINGID, STARTDATETIME = @STARTDATETIME, ENDDATETIME = @ENDDATETIME, TEACHER = @TEACHER WHERE ACTIVITYID = @ACTIVITYID", activity);
+            }
+        }
+
+        public void DeleteActivityBasis(int id)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Execute("DELETE FROM ACTIVITYBASIS WHERE ACTIVITYID = @Id", new { Id = id });
+            }
+        }
+
+        // ACTIVITY_SETTING CRUD Operations
+        public IEnumerable<ActivitySetting> GetAllActivitySettings()
+        {
+            using (var connection = GetConnection())
+            {
+                return connection.Query<ActivitySetting>("SELECT * FROM ACTIVITY_SETTING");
+            }
+        }
+
+        public ActivitySetting GetActivitySettingById(int id)
+        {
+            using (var connection = GetConnection())
+            {
+                return connection.QueryFirstOrDefault<ActivitySetting>("SELECT * FROM ACTIVITY_SETTING WHERE ACTIVITYSETTINGID = @Id", new { Id = id });
+            }
+        }
+
+        public void InsertActivitySetting(ActivitySetting setting)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Execute("INSERT INTO ACTIVITY_SETTING (NAME, IS_RECURRING, HOLDDAY, START_TIME, END_TIME) VALUES (@NAME, @IS_RECURRING, @HOLDDAY, @START_TIME, @END_TIME)", setting);
+            }
+        }
+
+        public void UpdateActivitySetting(ActivitySetting setting)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Execute("UPDATE ACTIVITY_SETTING SET NAME = @NAME, IS_RECURRING = @IS_RECURRING, HOLDDAY = @HOLDDAY, START_TIME = @START_TIME, END_TIME = @END_TIME WHERE ACTIVITYSETTINGID = @ACTIVITYSETTINGID", setting);
+            }
+        }
+
+        public void DeleteActivitySetting(int id)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Execute("DELETE FROM ACTIVITY_SETTING WHERE ACTIVITYSETTINGID = @Id", new { Id = id });
+            }
+        }
+
+        // GROUPMEMBERS CRUD Operations
+        public IEnumerable<GroupMembers> GetAllGroupMembers()
+        {
+            using (var connection = GetConnection())
+            {
+                return connection.Query<GroupMembers>("SELECT * FROM GROUPMEMBERS");
+            }
+        }
+
+        public GroupMembers GetGroupMemberByIds(string groupId, string memberId)
+        {
+            using (var connection = GetConnection())
+            {
+                return connection.QueryFirstOrDefault<GroupMembers>("SELECT * FROM GROUPMEMBERS WHERE GROUPID = @GroupId AND MEMBERID = @MemberId", new { GroupId = groupId, MemberId = memberId });
+            }
+        }
+
+        public void InsertGroupMember(GroupMembers member)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Execute("INSERT INTO GROUPMEMBERS (GROUPID, MEMBERID, JOINDATE, GROUPMEMBERLEVEL) VALUES (@GROUPID, @MEMBERID, @JOINDATE, @GROUPMEMBERLEVEL)", member);
+            }
+        }
+
+        public void UpdateGroupMember(GroupMembers member)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Execute("UPDATE GROUPMEMBERS SET JOINDATE = @JOINDATE, GROUPMEMBERLEVEL = @GROUPMEMBERLEVEL WHERE GROUPID = @GROUPID AND MEMBERID = @MEMBERID", member);
+            }
+        }
+
+        public void DeleteGroupMember(string groupId, string memberId)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Execute("DELETE FROM GROUPMEMBERS WHERE GROUPID = @GroupId AND MEMBERID = @MemberId", new { GroupId = groupId, MemberId = memberId });
+            }
+        }
+
+        // GROUPS CRUD Operations
+        public IEnumerable<Groups> GetAllGroups()
+        {
+            using (var connection = GetConnection())
+            {
+                return connection.Query<Groups>("SELECT * FROM GROUPS");
+            }
+        }
+
+        public Groups GetGroupById(string id)
+        {
+            using (var connection = GetConnection())
+            {
+                return connection.QueryFirstOrDefault<Groups>("SELECT * FROM GROUPS WHERE GROUPID = @Id", new { Id = id });
+            }
+        }
+
+        public void InsertGroup(Groups group)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Execute("INSERT INTO GROUPS (GROUPID, GROUPNAME, GROUPMANAGER) VALUES (@GROUPID, @GROUPNAME, @GROUPMANAGER)", group);
+            }
+        }
+
+        public void UpdateGroup(Groups group)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Execute("UPDATE GROUPS SET GROUPNAME = @GROUPNAME, GROUPMANAGER = @GROUPMANAGER WHERE GROUPID = @GROUPID", group);
+            }
+        }
+
+        public void DeleteGroup(string id)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Execute("DELETE FROM GROUPS WHERE GROUPID = @Id", new { Id = id });
+            }
+        }
+
+        // MEMBERS CRUD Operations
+        public IEnumerable<Members> GetAllMembers()
+        {
+            using (var connection = GetConnection())
+            {
+                return connection.Query<Members>("SELECT * FROM MEMBERS");
+            }
+        }
+
+        public Members GetMemberById(string id)
+        {
+            using (var connection = GetConnection())
+            {
+                return connection.QueryFirstOrDefault<Members>("SELECT * FROM MEMBERS WHERE MEMBERID = @Id", new { Id = id });
+            }
+        }
+
+        public void InsertMember(Members member)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Execute("INSERT INTO MEMBERS (MEMBERID, MEMBERNAME, BIRTHDAY, ZIPCODE, HOMEADDRESS, HOMETEL, MOBILETEL, EMAIL, BELIEFSTATUS, LOGINPASSWORD, SEX) VALUES (@MEMBERID, @MEMBERNAME, @BIRTHDAY, @ZIPCODE, @HOMEADDRESS, @HOMETEL, @MOBILETEL, @EMAIL, @BELIEFSTATUS, @LOGINPASSWORD, @SEX)", member);
+            }
+        }
+
+        public void UpdateMember(Members member)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Execute("UPDATE MEMBERS SET MEMBERNAME = @MEMBERNAME, BIRTHDAY = @BIRTHDAY, ZIPCODE = @ZIPCODE, HOMEADDRESS = @HOMEADDRESS, HOMETEL = @HOMETEL, MOBILETEL = @MOBILETEL, EMAIL = @EMAIL, BELIEFSTATUS = @BELIEFSTATUS, LOGINPASSWORD = @LOGINPASSWORD, SEX = @SEX WHERE MEMBERID = @MEMBERID", member);
+            }
+        }
+
+        public void DeleteMember(string id)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Execute("DELETE FROM MEMBERS WHERE MEMBERID = @Id", new { Id = id });
+            }
+        }
+
+        // STATUSES CRUD Operations
+        public IEnumerable<Statuses> GetAllStatuses()
+        {
+            using (var connection = GetConnection())
+            {
+                return connection.Query<Statuses>("SELECT * FROM STATUSES");
+            }
+        }
+
+        public Statuses GetStatusByIds(string itemName, string itemValue)
+        {
+            using (var connection = GetConnection())
+            {
+                return connection.QueryFirstOrDefault<Statuses>("SELECT * FROM STATUSES WHERE ITEMNAME = @ItemName AND ITEMVALUE = @ItemValue", new { ItemName = itemName, ItemValue = itemValue });
+            }
+        }
+
+        public void InsertStatus(Statuses status)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Execute("INSERT INTO STATUSES (ITEMNAME, ITEMVALUE, ITEMDESCRIPTION, ADMINPOWER) VALUES (@ITEMNAME, @ITEMVALUE, @ITEMDESCRIPTION, @ADMINPOWER)", status);
+            }
+        }
+
+        public void UpdateStatus(Statuses status)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Execute("UPDATE STATUSES SET ITEMDESCRIPTION = @ITEMDESCRIPTION, ADMINPOWER = @ADMINPOWER WHERE ITEMNAME = @ITEMNAME AND ITEMVALUE = @ITEMVALUE", status);
+            }
+        }
+
+        public void DeleteStatus(string itemName, string itemValue)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Execute("DELETE FROM STATUSES WHERE ITEMNAME = @ItemName AND ITEMVALUE = @ItemValue", new { ItemName = itemName, ItemValue = itemValue });
+            }
+        }
+    }
+}
