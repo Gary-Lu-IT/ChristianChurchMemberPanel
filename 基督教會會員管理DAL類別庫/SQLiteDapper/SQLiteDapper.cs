8d646622b310212d875ca7d93626aac9df979f10
Gary-Lu-IT <reimukaitoku@gmail.com>
2025-05-06 11:13:06 +0800

    加入主要連接的SQLITE資料庫檔並給予部分必填字串初始值

--- 基督教會會員管理DAL類別庫/SQLiteDapper/SQLiteDapper.cs
+++ 基督教會會員管理DAL類別庫/SQLiteDapper/SQLiteDapper.cs
@@ -24,7 +24,7 @@
         /// <summary>
         /// 主持人或講員
         /// </summary>
-        public string TEACHER { get; set; }
+        public string TEACHER { get; set; } = string.Empty;
     }
     /// <summary>
     /// 活動資料設定
@@ -38,23 +38,23 @@
         /// <summary>
         /// 活動名稱
         /// </summary>
-        public string NAME { get; set; }
+        public string NAME { get; set; } = null!;
         /// <summary>
         /// 是否定期舉行 (ONCE:僅一次,DAILY:每日,WEEK:每周,MONTH:每月,YEAR:每年)
         /// </summary>
-        public string IS_RECURRING { get; set; }
+        public string IS_RECURRING { get; set; } = null!;
         /// <summary>
         /// 舉辦日(ONCE填完整日期,WEEK填入星期幾,MONTH填入日期,YEAR填入幾月幾日,DAILY不適用)
         /// </summary>
-        public string HOLDDAY { get; set; }
+        public string HOLDDAY { get; set; } = null!;
         /// <summary>
         /// 開始時間
         /// </summary>
-        public string START_TIME { get; set; }
+        public string START_TIME { get; set; } = null!;
         /// <summary>
         /// 結束時間
         /// </summary>
-        public string END_TIME { get; set; }
+        public string END_TIME { get; set; } = null!;
     }
     /// <summary>
     /// 小組成員資料
@@ -64,11 +64,11 @@
         /// <summary>
         /// 小組ID
         /// </summary>
-        public string GROUPID { get; set; }
+        public string GROUPID { get; set; } = null!;
         /// <summary>
         /// 小組成員ID
         /// </summary>
-        public string MEMBERID { get; set; }
+        public string MEMBERID { get; set; } = null!;
         /// <summary>
         /// 加入時間
         /// </summary>
@@ -86,15 +86,15 @@
         /// <summary>
         /// 小組ID
         /// </summary>
-        public string GROUPID { get; set; }
+        public string GROUPID { get; set; } = null!;
         /// <summary>
         /// 小組名稱
         /// </summary>
-        public string GROUPNAME { get; set; }
+        public string GROUPNAME { get; set; } = null!;
         /// <summary>
         /// 組長會員ID
         /// </summary>
-        public string GROUPMANAGER { get; set; }
+        public string GROUPMANAGER { get; set; } = null!;
     }
     /// <summary>
     /// 會員資料
@@ -104,11 +104,11 @@
         /// <summary>
         /// 會員ID
         /// </summary>
-        public string MEMBERID { get; set; }
+        public string MEMBERID { get; set; } = null!;
         /// <summary>
         /// 會員姓名
         /// </summary>
-        public string MEMBERNAME { get; set; }
+        public string MEMBERNAME { get; set; } = null!;
         /// <summary>
         /// 生日
         /// </summary>
@@ -120,19 +120,19 @@
         /// <summary>
         /// 住家地址
         /// </summary>
-        public string HOMEADDRESS { get; set; }
+        public string HOMEADDRESS { get; set; } = string.Empty;
         /// <summary>
         /// 住家電話
         /// </summary>
-        public string HOMETEL { get; set; }
+        public string HOMETEL { get; set; } = string.Empty;
         /// <summary>
         /// 手機號碼
         /// </summary>
-        public string MOBILETEL { get; set; }
+        public string MOBILETEL { get; set; } = string.Empty;
         /// <summary>
         /// 電子郵件信箱
         /// </summary>
-        public string EMAIL { get; set; }
+        public string EMAIL { get; set; } = string.Empty;
         /// <summary>
         /// 信仰層級
         /// </summary>
@@ -140,7 +140,7 @@
         /// <summary>
         /// 登入密碼
         /// </summary>
-        public string LOGINPASSWORD { get; set; }
+        public string LOGINPASSWORD { get; set; } = string.Empty;
         /// <summary>
         /// 性別 1男0女
         /// </summary>
@@ -154,15 +154,15 @@
         /// <summary>
         /// 狀態ID
         /// </summary>
-        public string ITEMNAME { get; set; }
+        public string ITEMNAME { get; set; } = null!;
         /// <summary>
         /// 狀態值
         /// </summary>
-        public string ITEMVALUE { get; set; }
+        public string ITEMVALUE { get; set; } = null!;
         /// <summary>
         /// 狀態描述
         /// </summary>
-        public string ITEMDESCRIPTION { get; set; }
+        public string ITEMDESCRIPTION { get; set; } = null!;
         /// <summary>
         /// 套用此設定值的會員是否有系統管理員權限(部分類別不適用)
         /// </summary>
